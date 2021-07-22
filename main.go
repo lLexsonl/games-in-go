fe0c7f3553c5b0cdf1c294e7fa201362d8a2a4bc
Lexson <llalex.2045@gmail.com>
2021-07-21 23:15:09 -0500

    Molestango con las opciones del menu: El input de solo enteros

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+
 	games "github.com/lLexsonl/games-in-go/games"
 )
 
@@ -34,19 +38,50 @@ func main() {
 }
 
 func menu() {
+	fmt.Println()
 	for i, opt := range games_opt {
 		fmt.Printf("%d.. Para %s\n", i+1, opt)
 	}
+	fmt.Println()
+}
+
+func scan() (int, error) {
+	var err error
+	var opt int
+	var option int64
+
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		line := scanner.Text()
+		option, err = strconv.ParseInt(line, 10, 64)
+
+		if err != nil {
+			opt, err = 0, fmt.Errorf("solo ingresar numeros")
+		} else {
+			opt, err = int(option), nil
+		}
+
+	} else {
+		opt, err = 0, fmt.Errorf("error inesperado")
+	}
+	return opt, err
 }
 
 func choose() int {
 	for {
-		fmt.Println("Elige una opcion: ")
-		var option int
-		fmt.Scanln(&option)
+
+		option, err := scan()
+
+		if err != nil {
+			fmt.Println(err)
+			menu()
+			continue
+		}
+
 		if option > 0 && option <= g_size {
 			return option - 1
 		}
 		fmt.Println("Opcion no valida")
+		menu()
 	}
 }
